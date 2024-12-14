133b0f984e216363f8b42ac3795ff54a0dc12ab8
ggldnl <danielgigliotti99.dg@gmail.com>
2024-12-14 19:35:08 +0100

    Added renderer

---
+++ src/utils/renderer.py
@@ -0,0 +1,175 @@
+import numpy as np
+import pyrender
+
+
+class Renderer:
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def _load_mesh(model_in):
+        return trimesh.load(model_in, process=False)
+
+    def render_glcam(self, model_in, K, Rt, scale=1.0, rend_size=(512, 512),
+                     light_trans=np.array([[0], [100], [0]]), flat_shading=False):
+        """
+        OpenGL's coordinate system conventions: Z-axis is forward and the Y-axis is up.
+        """
+
+        # Mesh creation
+        mesh = self._load_mesh(model_in)
+        pr_mesh = pyrender.Mesh.from_trimesh(mesh)
+
+        # Scene creation
+        scene = pyrender.Scene()
+        scene.add(pr_mesh)
+
+        # Calculate camera intrinsics
+        fx, fy = K[0][0] * scale, K[1][1] * scale
+        cx, cy = K[0][2] * scale, K[1][2] * scale
+
+        # Camera setup
+        cam = pyrender.IntrinsicsCamera(fx, fy, cx, cy, znear=0.1, zfar=100000)
+        cam_pose = np.eye(4)
+        cam_pose[:3, :3] = Rt[:3, :3].T
+        cam_pose[:3, 3] = -Rt[:3, :3].T.dot(Rt[:, 3])
+        scene.add(cam, pose=cam_pose)
+
+        # Light setup
+        light = pyrender.DirectionalLight(color=[1.0, 1.0, 1.0], intensity=10.0)
+        light_pose = cam_pose.copy()
+        light_pose[0:3, :] += light_trans
+        scene.add(light, pose=light_pose)
+
+        # Rendering
+        renderer = pyrender.OffscreenRenderer(viewport_width=rend_size[1],
+                                              viewport_height=rend_size[0],
+                                              point_size=1.0)
+        flags = pyrender.constants.RenderFlags.FLAT if flat_shading else 0
+        color, depth = renderer.render(scene, flags=flags)
+
+        # Convert RGB to BGR
+        color = color[:, :, [2, 1, 0]]
+
+        return depth, color
+
+    def render_cvcam(self, model_in, K=None, Rt=None, scale=1.0, rend_size=(512, 512),
+                      light_trans=np.array([[0], [100], [0]]), flat_shading=False):
+        """
+        OpenCV's format.
+        """
+
+        if K is None:
+            K = np.array([[2000, 0, 256],
+                    [0, 2000, 256],
+                    [0, 0, 1]], dtype=np.float64)
+        if Rt is None:
+            Rt = np.array([[1, 0, 0, 0],
+                    [0, 1, 0, 0],
+                    [0, 0, 1, 0]], dtype=np.float64)
+
+        # Transform Rt for OpenCV to OpenGL
+        R_cv2gl = np.array([[1, 0, 0],
+                            [0, -1, 0],
+                            [0, 0, -1]])
+        Rt_cv = R_cv2gl.dot(Rt)
+
+        return self.render_glcam(model_in, K, Rt_cv, scale, rend_size, light_trans, flat_shading)
+
+    def render_orthcam(self, model_in, xy_mag, rend_size, flat_shading=False, zfar=10000, znear=0.05):
+        """
+        Orthographic cameras, no perspective distortion.
+        """
+
+        # Mesh creation
+        mesh = self._load_mesh(model_in)
+        pr_mesh = pyrender.Mesh.from_trimesh(mesh)
+
+        # Scene creation
+        scene = pyrender.Scene()
+        scene.add(pr_mesh)
+
+        # Camera setup
+        if isinstance(xy_mag, float):
+            cam = pyrender.OrthographicCamera(xmag=xy_mag, ymag=xy_mag, znear=znear, zfar=zfar)
+        elif isinstance(xy_mag, tuple):
+            cam = pyrender.OrthographicCamera(xmag=xy_mag[0], ymag=xy_mag[1], znear=znear, zfar=zfar)
+        else:
+            raise ValueError("Error: xy_mag should be a float or a tuple")
+
+        scene.add(cam, pose=np.eye(4))
+
+        # Light setup
+        light = pyrender.DirectionalLight(color=[1.0, 1.0, 1.0], intensity=10.0)
+        scene.add(light, pose=np.eye(4))
+
+        # Rendering offscreen
+        renderer = pyrender.OffscreenRenderer(viewport_width=rend_size[1],
+                                              viewport_height=rend_size[0],
+                                              point_size=1.0)
+        flags = pyrender.constants.RenderFlags.FLAT if flat_shading else 0
+        color, depth = renderer.render(scene, flags=flags)
+
+        # Convert RGB to BGR
+        color = color[:, :, [2, 1, 0]]
+
+        # Fix pyrender BUG of depth rendering, pyrender version: 0.1.43
+        if pyrender.__version__() <= 0.1:
+            depth[depth != 0] = (zfar + znear - ((2.0 * znear * zfar) / depth[depth != 0])) / (zfar - znear)
+            depth[depth != 0] = ((depth[depth != 0] + (zfar + znear) / (zfar - znear)) * (zfar - znear)) / 2.0
+
+        return depth, color
+
+if __name__ == "__main__":
+
+    import matplotlib.pyplot as plt
+    from pathlib import Path
+    import trimesh
+    import json
+
+    import src.config.config as config
+
+    # Instantiate the renderer
+    renderer = Renderer()
+
+    # Read the params file (given in the dataset) and take a config dict
+    params_path = Path(config.DATA_DIR, "100/1_neutral/params.json")
+    with open(params_path) as f:
+        params = json.load(f)
+
+    # There is a parameter dict in the same file for each image of the rig (60 in total, from 0 to 59).
+    # This for each expression of each user.
+    id = 0
+    K = np.array(params[f'{id}_K'])
+    Rt = np.array(params[f'{id}_Rt'])
+
+    # Render using the OpenGL camera method
+    mesh_path = Path(config.DATA_DIR, '100/models_reg/1_neutral.obj')
+
+    depth, color = renderer.render_cvcam(
+        model_in=mesh_path,
+        K=K,
+        Rt=Rt,
+        scale=1.0,
+        rend_size=(512, 512),
+        light_trans=np.array([[0], [100], [0]]),
+        flat_shading=False,
+    )
+
+    # Display the results
+    plt.figure(figsize=(10, 5))
+
+    # Rendered color image
+    plt.subplot(1, 2, 1)
+    plt.imshow(color)
+    plt.title("Rendered Color")
+    plt.axis("off")
+
+    # Rendered depth map
+    plt.subplot(1, 2, 2)
+    plt.imshow(depth, cmap="gray")
+    plt.title("Rendered Depth")
+    plt.axis("off")
+
+    plt.show()
