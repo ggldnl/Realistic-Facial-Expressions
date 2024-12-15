7a92b2c53d04ae310cdca6b5dfed4ce52bedb297
ggldnl <danielgigliotti99.dg@gmail.com>
2024-12-15 19:48:44 +0100

    Added training and model sections

--- src/config/config.py
+++ src/config/config.py
@@ -18,8 +18,18 @@ RESOURCE_URL = r"https://drive.google.com/drive/folders/14aQeK7S35FP5Z9EkJ6e6TCZ
 DOWNLOAD_SOURCE = 'drive'
 
 # Download, skip or check if all the files are there before downloading
-DOWNLOAD = 'infer'
+DOWNLOAD = 'no'
 
 # Splitting percentage
 TRAIN_SPLIT = 0.8
 TEST_SPLIT = 0.1
+
+# ----------------------------------- Model ---------------------------------- #
+LAYERS_IN = [26404, 10000, 5000, 2500]  # 26404 being the size of the meshes in our dataset
+LATENT_SIZE = 512
+TEXT_EMBED_SIZE = 512
+
+# --------------------------------- Training --------------------------------- #
+
+EPOCHS = 50
+PATIENCE = 20
