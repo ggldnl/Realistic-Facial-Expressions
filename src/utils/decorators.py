79330dc71ef6acaf93000e85953f4e957ffcbb0c
ggldnl <danielgigliotti99.dg@gmail.com>
2024-12-15 18:17:01 +0100

    Added fixme decorator

--- src/utils/decorators.py
+++ src/utils/decorators.py
@@ -11,3 +11,14 @@ def deprecated(func):
         )
         return func(*args, **kwargs)
     return wrapper
+
+def deprecated(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        warnings.warn(
+            f"{func.__name__} has some bugs and needs to be fixed.",
+            category=DeprecationWarning,
+            stacklevel=2,
+        )
+        return func(*args, **kwargs)
+    return wrapper
